b161cd29b817ddb259244def0b13571de81ffe0b
Rohini <rohini.vaidya@parjanya.org>
2020-05-05 13:11:15 +0530

    Initial Commit

---
+++ COreDataExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COreDataExample
+//
+//  Created by Admin on 5/5/20.
+//  Copyright © 2020 Rohini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
